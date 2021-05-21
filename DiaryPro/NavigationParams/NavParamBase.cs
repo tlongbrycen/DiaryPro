e271f160a4081bebdfc1e1adcdaa569a07a798dc
tlong <tlong@brycen.co.jp>
2021-05-21 19:00:56 +0900

    Panel Color Selected

---
+++ DiaryPro/NavigationParams/NavParamBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace DiaryPro.NavigationParams
+{
+    class NavParamBase
+    {
+        public Type SourcePage;
+        public Type TargetPage;
+        public string ExtraCommand;
+
+        public NavParamBase()
+        {
+            SourcePage = typeof(MainPage);
+            TargetPage = typeof(MainPage);
+            ExtraCommand = "none";
+        }
+    }
+}
---
+++ DiaryPro/Views/HomePage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using DiaryPro.NavigationParams;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DiaryPro
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            SetTextDateTime();
+            if (e.Parameter != null)
+            {
+                NavParamBase navParam = (NavParamBase)e.Parameter;
+                if(navParam.SourcePage.Equals(typeof(MainPage)) &&
+                    navParam.TargetPage.Equals(typeof(HomePage)))
+                {
+                    string request = ((NavParamMainToHome)navParam).ExtraCommand;
+                }
+            }
+        }
+
+        private void SetTextDateTime()
+        {
+            txtDateTime.Text = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+        }
+           
+    }
+}
